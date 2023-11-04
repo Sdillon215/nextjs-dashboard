f7b271ac9a7c6e38a2887fa8c39dd3499de4cd70
Sean <sdillon215@gmail.com>
2023-11-04 00:35:44 -0400

    feat(component streaming): revenue and latest invoicecomponent streams isolated

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,19 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 // Data Fetching
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    console.log("revenue");
-    console.log(revenue);
-
-    const latestInvoices = await fetchLatestInvoices();
-    console.log("latest invoices");
-    console.log(latestInvoices);
-
     const cardData = await fetchCardData();
     console.log("card data");
     console.log(cardData);
@@ -31,8 +25,12 @@ export default async function Page() {
                 <Card title="Total Customers" value={cardData.numberOfCustomers} type="customers" />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue} />
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
